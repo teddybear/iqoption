fe69c2fb25e7bbfd406689964dff3f8a5c29c7c9
Alexey Ismailov <comload@gmail.com>
2017-04-04 10:28:47 +0500

    tests

---
+++ iqopt/tests/api_test.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+import pytest
+from ..app import app as appl
+import json
+
+
+@pytest.fixture(scope="session")
+def app(request):
+    _app = appl
+    ctx = _app.app_context()
+    ctx.push()
+
+    def teardown():
+        ctx.pop()
+
+    request.addfinalizer(teardown)
+    return _app
+
+
+@pytest.fixture(scope="function")
+def client(app):
+    return app.test_client()
+
+
+def test_smth():
+    assert 1 == 1
+
+
+def test_translate(client):
+    print dir(client)
+    assert client.post(
+        "/translate",
+        data=json.dumps({"text": u"Текст", "lang": "en-ru"}),
+        content_type="application/json"
+    ).status_code == 200
+
+
+def test_stats(client):
+    assert client.post(
+        "/translate/stats",
+        data=json.dumps({"text": u"Текст", "lang": "en-ru"}),
+        content_type="application/json"
+    ).status_code == 200
